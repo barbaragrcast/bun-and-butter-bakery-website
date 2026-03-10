ac3455857dc4bff48dd02388ebe53d3d3ce8df01
barbaragrcast <barbaragrcast@gmail.com>
2026-03-09 19:34:42 -0500

    about

--- client/src/pages/about.jsx
+++ client/src/pages/about.jsx
@@ -2,13 +2,17 @@ import React from "react";
 
 const About = () => {
   return (
-    <>
-      <div id="about">
-        <h1> About Our Bakery </h1>
-        <h3> Welcome to Bun & Butter Bakery! We bake fresh bread, cookies, brownies, and pastries every day using simple, quality ingredients.
-            Our goal is to bring a little sweetness and warmth to your day. Stop by for something fresh and delicious</h3>
+    <div id="about">
+      <div className="about-card">
+        <h1>About Our Bakery</h1>
+        <h3>
+          Welcome to <span className="highlight">Bun & Butter Bakery!</span> 
+          We bake fresh bread, cookies, brownies, and pastries every day using 
+          simple, quality ingredients. Our goal is to bring a little sweetness 
+          and warmth to your day. Stop by for something fresh and delicious.
+        </h3>
       </div>
-    </>
+    </div>
   );
 };
 
