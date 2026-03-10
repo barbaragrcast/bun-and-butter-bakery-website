8d0ee71ac1649356203f077f9710700e3483f25e
barbaragrcast <barbaragrcast@gmail.com>
2026-03-09 21:00:02 -0500

    s

--- client/src/pages/contact.jsx
+++ client/src/pages/contact.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import ContactForm from "../components/contactForm"
+import ContactForm from "../components/contactForm";
 
 const Contact = () => {
   return (
     <div id="contact-page">
-      <ContactForm />
-      {}
+      <div id="contact-container">
+        <h1 className="parisienne-regular">Contact Us</h1>
+        <p>We’d love to hear from you! Questions, feedback, or just to say hi 😊</p>
+        <ContactForm />
+      </div>
     </div>
   );
 };
